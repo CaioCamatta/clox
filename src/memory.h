cdd2f39f40f44d20a4bf2ec9b2498d1672375dc0
Caio Camatta <caioz.co@gmail.com>
2023-02-25 15:30:54 -0500

    Fix memory.h - add func def

--- src/memory.h
+++ src/memory.h
@@ -3,15 +3,17 @@
 
 #include "common.h"
 
-#define GROW_CAPACITY(capacity) \ 
+#define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity)*2)
 
 // This macro takes care of getting the size of the array's element type
 // & casting the resulting void* back to a pointer of the right type.
-#define GROW_ARRAY(type, pointer, oldCount, newCount) \ 
+#define GROW_ARRAY(type, pointer, oldCount, newCount) \
     (type*)reallocate(pointer, sizeof(type) * (oldCount), sizeof(type) * (newCount))
 
-#define FREE_ARRAY(type, pointer, oldCount) \ 
+#define FREE_ARRAY(type, pointer, oldCount) \
     reallocate(pointer, sizeof(type) * (oldCount), 0)
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
 #endif
