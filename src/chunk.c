34dbc1eca268c013e59bcc6694a7a1a437376b29
Caio Camatta <ccoelho6@uwo.ca>
2023-03-08 20:31:42 -0500

    Chapter 14 done

--- src/chunk.c
+++ src/chunk.c
@@ -9,18 +9,22 @@ void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
 /* Write to dynamic array. This function will update the size of the array if needed. */
-void writeChunk(Chunk* chunk, uint8_t byte) {
+void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+        // Keep lines in array parallel with code
+        chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
     }
 
     chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
     chunk->count++;
 }
 
@@ -33,6 +37,7 @@ int addConstant(Chunk* chunk, Value value) {
 /* Delete array and free its memory */
 void freeChunk(Chunk* chunk) {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);  // Zero out the fields to clean up the chunk
 }
--- src/debug.c
+++ src/debug.c
@@ -33,6 +33,12 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
 int disassembleInstruction(Chunk* chunk, int offset) {
     printf("%04d ", offset);
 
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
+        printf("  || ");  // Instruction comes from same line as the previous one
+    } else {
+        printf("%4d ", chunk->lines[offset]);  // New line of code
+    }
+
     uint8_t instruction = chunk->code[offset];
 
     switch (instruction) {
