eeb5d46f4267d48b17ef8979666ed8e24ca8a25a
Caio Camatta <ccoelho6@uwo.ca>
2023-03-30 22:29:32 -0400

    17.4 Done

--- src/compiler.c
+++ src/compiler.c
@@ -1,26 +1,175 @@
 #include "compiler.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-    initScanner(source);
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;  // Used for error handling to avoid error cascades.
+} Parser;
 
-    // Temporary code
-    // The compiler requests tokens from the scanner as needed. Clox's grammar only requires a single token of lookahead
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT,  // =
+    PREC_OR,          // or
+    PREC_AND,         // and
+    PREC_EQUALITY,    // == !=
+    PREC_COMPARISON,  // < > <= >=
+    PREC_TERM,        // + -
+    PREC_FACTOR,      // * /
+    PREC_UNARY,       // ! -
+    PREC_CALL,        // . ()
+    PREC_PRIMARY
+} Precedence;
 
-        if (token.type == TOKEN_EOF) break;
+Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
+
+static void errorAt(Token* token, const char* message) {
+    if (parser.panicMode) return;
+
+    // Enter panic mode when we find an error
+    parser.panicMode = true;
+
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // Nothing.
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
     }
+
+    fprintf(stderr, ": %s\n", message);
+    parser.hadError = true;
+}
+
+// Error at the location of the token just consumed
+static void error(const char* message) {
+    errorAt(&parser.previous, message);
+}
+
+// Errors at token to-be-consumed
+static void errorAtCurrent(const char* message) {
+    errorAt(&parser.current, message);
+}
+
+// Scan tokens & advance parser while current token is not a TOKEN_ERROR
+static void advance() {
+    parser.previous = parser.current;
+
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        // In clox, the parser reports errors, not the scanner
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+// Consume provided type & advance, or error
+static void consume(TokenType type, const char* message) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(message);
+}
+
+/* Write given byte (opcode or operand) and record line information. */
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+/* Emit two bytes (convinience function meant for opcode + operand) */
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+static uint8_t makeConstant(Value value) {
+    int constant = addConstant(currentChunk(), value);
+    // OP_CONSTANT uses a single byte for the index operand
+    if (constant > UINT8_MAX) {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+
+    return (uint8_t)constant;
+}
+
+static void emitConstant(Value value) {
+    emitBytes(OP_CONSTANT, makeConstant(value));
+}
+
+static void endCompiler() {
+    emitReturn();
+}
+
+static void grouping() {
+    // Assume initial '(' has already been consumed
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number() {
+    // Assume the token for the number literal has already been consumed
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void unary() {
+    TokenType operatorType = parser.previous.type;
+
+    // Compile operand
+    parsePrecedence(PREC_UNARY);  // Only parse unary or higher. Prevents cases such as "-a.b + c" where we wouldn't want all of "a.b + c" to be negated
+
+    // Emit the operator instruction
+    switch (operatorType) {
+        case TOKEN_MINUS:
+            // Emit negation *after* operand, so we can pop operand, negate it, and push it back to the stack
+            emitByte(OP_NEGATE);
+            break;
+        default:
+            return;  // Impossible.
+    }
+}
+
+/* Start a current level and parse any expression at the given precedence level or higher. */
+static void parsePrecedence(Precedence precedence) {
+    // What goes here?
+}
+
+static void expression() {
+    parsePrecedence(PREC_ASSIGNMENT);
+}
+
+/* Read char* source and write the code to chunk. */
+bool compile(const char* source, Chunk* chunk) {
+    initScanner(source);
+    compilingChunk = chunk;
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
+    endCompiler();
+    return !parser.hadError;
 }
