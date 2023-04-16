98befcac1d12acaa2af135e67cdb97185dc9a82f
Caio Camatta <caioz.co@gmail.com>
2023-04-16 14:54:34 -0400

    20.4 done

--- src/object.c
+++ src/object.c
@@ -25,28 +25,42 @@ static Obj* allocateObject(size_t size, ObjType type) {
 
 /* Create new ObjString on the heap and initialize its fields.
 Similar to a constructor in OOP.*/
-static ObjString* allocateString(char* chars, int length) {
+static ObjString* allocateString(char* chars, int length,
+                                 uint32_t hash) {
     // Call the "base class" constructor
     ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
     string->length = length;
     string->chars = chars;
+    string->hash = hash;
     return string;
+}
+
+/* FNV-1a hash */
+static uint32_t hashString(const char* key, int length) {
+    uint32_t hash = 2166136261u;
+    for (int i = 0; i < length; i++) {
+        hash ^= (u_int8_t)key[i];
+        hash *= 16777619;
+    }
+    return hash;
 }
 
 /* Allocate string in heap from C string passed in, essentially claiming ownership of the string given.
 This function is used for concatenation.*/
 ObjString* takeString(char* chars, int length) {
-    return allocateString(chars, length);
+    uint32_t hash = hashString(chars, length);
+    return allocateString(chars, length, hash);
 }
 
 /* Copy a string to heap */
 ObjString* copyString(const char* chars, int length) {
+    uint32_t hash = hashString(chars, length);
     char* heapChars = ALLOCATE(char, length + 1);
     // Copy string from the lexeme (in the monolithic source string)
     memcpy(heapChars, chars, length);
     // Terminate string to facilitate conversion to c string
     heapChars[length] = '\0';
-    return allocateString(heapChars, length);
+    return allocateString(heapChars, length, hash);
 }
 
 void printObject(Value value) {
